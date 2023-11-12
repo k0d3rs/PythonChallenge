07a410923658eb6ce7b263f27d5393f591761198
k0d3rs <victormendez1977@yahoo.es>
2023-11-12 16:11:38 +0100

    Python Challenge Initial Commit

---
+++ 00.py
@@ -0,0 +1,8 @@
+# http://www.pythonchallenge.com/pc/def/0.html
+
+nextUrl = "http://www.pythonchallenge.com/pc/def/0.html"
+print(nextUrl.replace("0", str(2**38)))
+exit(0)
+
+# Answer http://www.pythonchallenge.com/pc/def/274877906944.html
+
---
+++ 01.py
@@ -0,0 +1,13 @@
+# http://www.pythonchallenge.com/pc/def/map.html
+
+inputText ="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+outputMap = map(lambda x: chr(ord(x) + 2), inputText)
+outputText = "".join(outputMap)
+print(outputText)
+
+inputUrl = "map"
+outputUrl = "".join(map(lambda x: chr(ord(x) + 2), inputUrl))
+print(outputUrl)
+
+# Answer http://www.pythonchallenge.com/pc/def/ocr.html
+
